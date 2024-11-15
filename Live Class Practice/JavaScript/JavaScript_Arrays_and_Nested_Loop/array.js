3a70f78b77428ade036f4f97c7776feb966a2581
shorifulsobuj <sobujaiom126@gmail.com>
2024-11-15 20:13:01 +0600

    first commit

---
+++ Live Class Practice/JavaScript/JavaScript_Arrays_and_Nested_Loop/array.js
@@ -0,0 +1,36 @@
+// let str = "Hello, World!";
+let names = ["John", "Alice", "Bob", "Hello World!"];
+
+// console.log(names[names.length - 1]);
+// delete.log(names[names.length - 1]);
+
+// names.pop();
+// names.pop();
+// names.pop();
+
+// names.unshift("ZZZ");
+
+names.shift("Adss");
+
+
+26:02 minit done
+
+console.log(names);
+
+// names[10] = "Sobuj";
+// delete names[1];
+
+// names[12] = "Aohn";
+
+// names[names.length] = "Sobuj";
+// names[names.length] = "Akondo";
+
+// Property -> information
+// Method -> action
+
+// names.push("Islam");
+
+// console.log(names);
+
+// console.log(names[1]);
+// int, 5 -> 20 byte
